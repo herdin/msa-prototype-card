44e912c286b707676f230a2383a50f15f8c52656
joshin <joshin0724@gmail.com>
2020-05-06 17:25:15 +0900

    application yml update

--- src/main/java/card/MainApplication.java
+++ src/main/java/card/MainApplication.java
@@ -3,9 +3,15 @@ package card;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
+import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
 
 @SpringBootApplication
 @EnableCircuitBreaker
+/**
+ * http://localhost:8090/hystrix
+ * http://localhost:8090/actuator/hystrix.stream
+ */
+@EnableHystrixDashboard
 public class MainApplication {
 
     public static void main(String[] args) {
